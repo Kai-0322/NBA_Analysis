99dc9a8edec1384b18a7a9222a97ff20b625e625
McKinley Card <ktownkinley@gmail.com>
2024-03-28 20:56:11 -0600

    test

---
+++ MJ_LJ_stats_analysis.py
@@ -0,0 +1,10 @@
+#cell 1
+import pandas as pd
+from matplotlib import pyplot as plt
+#cell 2
+nba_df = pd.read_csv("NBA_Data/NBA Player Stats(1950 - 2022).csv")
+nba_df = nba_df[["Season", "Player", "G", "MP", "PTS", "TRB", "AST", "STL", "BLK", "TOV"]]
+mj_nba_df = nba_df.loc[nba_df["Player"] == "Michael Jordan"]
+lj_nba_df = nba_df.loc[nba_df["Player"] == "LeBron James"]
+specific_player_df = nba_df.loc[(nba_df["Player"] == "LeBron James") | (nba_df["Player"] == "Michael Jordan")]
+avg_games_df = specific_player_df.groupby("Player")["G"].mean()
