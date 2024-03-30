0aadd4f578f47dee51673b5f5cfba76ff03795c6
Kai-0322 <tsetsen98@gmail.com>
2024-03-30 11:14:23 -0600

    updated py file

--- MJ_LJ_stats_analysis.py
+++ MJ_LJ_stats_analysis.py
@@ -4,24 +4,28 @@ from matplotlib import pyplot as plt
 import numpy as np
 #cell 2
 nba_df = pd.read_csv("NBA_Data/NBA Player Stats(1950 - 2022).csv")
-nba_df = nba_df[["Season", "Player", "G", "MP", "PTS", "TRB", "AST", "STL", "BLK", "TOV"]]
+nba_df = nba_df[["Season", "Player", "G", "FG%", "3P%", "2P%", "MP", "PTS", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV"]]
 #cell 3
 specific_player_df = nba_df.loc[(nba_df["Player"] == "LeBron James") | (nba_df["Player"] == "Michael Jordan")]
 specific_player_df.head()
 #cell 4
-#Lifetime Total Stats of each player (as of 2022 LeBron James)
-total_games = specific_player_df["G"].groupby(specific_player_df["Player"]).sum()
-total_mins = specific_player_df["MP"].groupby(specific_player_df["Player"]).sum()
-total_pts = specific_player_df["PTS"].groupby(specific_player_df["Player"]).sum()
-total_reb = specific_player_df["TRB"].groupby(specific_player_df["Player"]).sum()
-total_ast = specific_player_df["AST"].groupby(specific_player_df["Player"]).sum()
-total_stl = specific_player_df["STL"].groupby(specific_player_df["Player"]).sum()
-total_blk = specific_player_df["BLK"].groupby(specific_player_df["Player"]).sum()
-total_tov = specific_player_df["TOV"].groupby(specific_player_df["Player"]).sum()
-total_summary_df = pd.DataFrame({"Total Games Played": total_games, "Total Mins Played": total_mins, "Total Points Scored": total_pts,
-                           "Total Rebounds": total_reb, "Total Assists": total_ast, "Total Steals": total_stl, "Total Blocks": total_blk, 
-                           "Total Turnovers": total_tov})
-total_summary_df
+#Lifetime Average Stats of each player (as of 2022 LeBron James)
+mean_games = specific_player_df["G"].groupby(specific_player_df["Player"]).mean()
+mean_fgp = specific_player_df["FG%"].groupby(specific_player_df["Player"]).mean()
+mean_3pc = specific_player_df["3P%"].groupby(specific_player_df["Player"]).mean()
+mean_2pc = specific_player_df["2P%"].groupby(specific_player_df["Player"]).mean()
+mean_mins = specific_player_df["MP"].groupby(specific_player_df["Player"]).mean()
+mean_pts = specific_player_df["PTS"].groupby(specific_player_df["Player"]).mean()
+mean_reb = specific_player_df["TRB"].groupby(specific_player_df["Player"]).mean()
+mean_ast = specific_player_df["AST"].groupby(specific_player_df["Player"]).mean()
+mean_stl = specific_player_df["STL"].groupby(specific_player_df["Player"]).mean()
+mean_blk = specific_player_df["BLK"].groupby(specific_player_df["Player"]).mean()
+mean_tov = specific_player_df["TOV"].groupby(specific_player_df["Player"]).mean()
+mean_summary_df = pd.DataFrame({"Avg. Games Played": mean_games, "Avg. Mins Played": mean_mins, "Avg. Points Scored": mean_pts,
+                                "Avg. Field Goal %": mean_fgp, "Avg. 3 Point %": mean_3pc, "Avg. 2 Point %": mean_2pc,
+                           "Avg. Rebounds": mean_reb, "Avg. Assists": mean_ast, "Avg. Steals": mean_stl, "Avg. Blocks": mean_blk, 
+                           "Avg. Turnovers": mean_tov})
+mean_summary_df
 #cell 5
 #Lifetime Average Stats of each player (as of 2022 LeBron James)
 mean_games = specific_player_df["G"].groupby(specific_player_df["Player"]).mean()
@@ -37,35 +41,34 @@ mean_summary_df = pd.DataFrame({"Avg. Games Played": mean_games, "Avg. Mins Play
                            "Avg. Turnovers": mean_tov})
 mean_summary_df
 #cell 6
-each_game_stats = pd.read_csv("NBA_Data/NBA Player Box Score Stats(1950 - 2022).csv")
-each_game_stats.head()
+each_game_stats = pd.read_csv("NBA_Data/MJ_LJ_Games.csv")
+each_game_stats.head()  
 #cell 7
-mj_each_game = each_game_stats.loc[each_game_stats["PLAYER_NAME"] == "Michael Jordan"]
-mj_each_game
+each_game_stats = each_game_stats[["Season", "Player", "MATCHUP", "WL", "PTS", "FG%", "OREB", "DREB", "AST", "STL", "BLK", "TOV"]]
+each_game_stats.head()
 #cell 8
+#Check for na values
+each_game_stats.count()
+#cell 9 
+each_game_stats.dropna(inplace=True)
+#cell 10
+each_game_stats.count()
+#cell 11 
+mj_each_game = each_game_stats.loc[each_game_stats["Player"] == "Michael Jordan"]
+mj_each_game
+#cell 12
 champ_mj_avg = mj_each_game[(mj_each_game["Season"] == 1991)]["PTS"].mean()
 champ_mj_avg
-#cell 9 
-lj_each_game = each_game_stats.loc[each_game_stats["PLAYER_NAME"] == "LeBron James"]
+#cell 13
+lj_each_game = each_game_stats.loc[each_game_stats["Player"] == "LeBron James"]
 lj_each_game
-#cell 10
+#cell 14
 champ_lj_avg = lj_each_game[(lj_each_game["Season"] == 2012)]["PTS"].mean()
 champ_lj_avg
-#cell 11 
+#cell 15
 #Average Points each player scored when they won championship (around the same age)
 champ_avg = pd.DataFrame({"Avg. Points MJ Scored": champ_mj_avg, "Avg. Points LJ Scored": champ_lj_avg}, index = [0])
 champ_avg
-#cell 12
+#cell 16
 mj_pts = mj_each_game.loc[mj_each_game["Season"] == 1991]
 mj_pts
-#cell 13
-x_axis = np.arange(len(mj_pts["MATCHUP"]))
-tick_locations = [value+0.4 for value in x_axis]
-plt.figure(figsize=(20,4))
-plt.plot(x_axis, mj_pts["PTS"], color='r')
-plt.xticks(tick_locations, mj_pts["MATCHUP"], rotation="vertical")
-plt.show()
-#cell 14
-mj_nba_df = nba_df.loc[nba_df["Player"] == "Michael Jordan"]
-#cell 15
-lj_nba_df = nba_df.loc[nba_df["Player"] == "LeBron James"]
