2c85ff4a0f56b0aab45a98e93835ce7b6227a84e
McKinley Card <ktownkinley@gmail.com>
2024-03-30 12:07:15 -0600

    adding plots for means

--- MJ_LJ_stats_analysis.py
+++ MJ_LJ_stats_analysis.py
@@ -27,51 +27,48 @@ mean_summary_df = pd.DataFrame({"Avg. Games Played": mean_games, "Avg. Mins Play
                            "Avg. Turnovers": mean_tov})
 mean_summary_df
 #cell 5
-#Lifetime Average Stats of each player (as of 2022 LeBron James)
-mean_games = specific_player_df["G"].groupby(specific_player_df["Player"]).mean()
-mean_mins = specific_player_df["MP"].groupby(specific_player_df["Player"]).mean()
-mean_pts = specific_player_df["PTS"].groupby(specific_player_df["Player"]).mean()
-mean_reb = specific_player_df["TRB"].groupby(specific_player_df["Player"]).mean()
-mean_ast = specific_player_df["AST"].groupby(specific_player_df["Player"]).mean()
-mean_stl = specific_player_df["STL"].groupby(specific_player_df["Player"]).mean()
-mean_blk = specific_player_df["BLK"].groupby(specific_player_df["Player"]).mean()
-mean_tov = specific_player_df["TOV"].groupby(specific_player_df["Player"]).mean()
-mean_summary_df = pd.DataFrame({"Avg. Games Played": mean_games, "Avg. Mins Played": mean_mins, "Avg. Points Scored": mean_pts,
-                           "Avg. Rebounds": mean_reb, "Avg. Assists": mean_ast, "Avg. Steals": mean_stl, "Avg. Blocks": mean_blk, 
-                           "Avg. Turnovers": mean_tov})
-mean_summary_df
-#cell 6
 each_game_stats = pd.read_csv("NBA_Data/MJ_LJ_Games.csv")
 each_game_stats.head()
-#cell 7  
+#cell 6  
 each_game_stats.rename(columns={"PLAYER_NAME": "Player", "FG_PCT": "FG%", "FG3_PCT": "3P%"}, inplace=True)
 each_game_stats
-#cell 8
+#cell 7
 each_game_stats = each_game_stats[["Season", "Player", "MATCHUP", "WL", "PTS", "FG%", "OREB", "DREB", "AST", "STL", "BLK", "TOV"]]
 each_game_stats.head()
-#cell 9
+#cell 8
 #Check for na values
 each_game_stats.count()
-#cell 10 
+#cell 9 
 each_game_stats.dropna(inplace=True)
-#cell 11
+#cell 10
 each_game_stats.count()
-#cell 12 
+#cell 11 
 mj_each_game = each_game_stats.loc[each_game_stats["Player"] == "Michael Jordan"]
 mj_each_game
-#cell 13
+#cell 12
 champ_mj_avg = mj_each_game[(mj_each_game["Season"] == 1991)]["PTS"].mean()
 champ_mj_avg
-#cell 14
+#cell 13
 lj_each_game = each_game_stats.loc[each_game_stats["Player"] == "LeBron James"]
 lj_each_game
-#cell 15
+#cell 14
 champ_lj_avg = lj_each_game[(lj_each_game["Season"] == 2012)]["PTS"].mean()
 champ_lj_avg
-#cell 16
+#cell 15
 #Average Points each player scored when they won championship (around the same age)
 champ_avg = pd.DataFrame({"Avg. Points MJ Scored": champ_mj_avg, "Avg. Points LJ Scored": champ_lj_avg}, index = [0])
 champ_avg
-#cell 17
+#cell 16
 mj_pts = mj_each_game.loc[mj_each_game["Season"] == 1991]
 mj_pts
+#cell 17
+summary_values_chart = mean_summary_df[["Avg. Games Played", "Avg. Mins Played", "Avg. Points Scored", "Avg. Rebounds", "Avg. Assists", "Avg. Steals", "Avg. Blocks", "Avg. Turnovers"]]
+summary_values_chart.plot(kind="bar")
+plt.legend(loc="upper center", bbox_to_anchor=(0.45,0.8))
+plt.show()
+#cell 18
+summary_per_chart = mean_summary_df[["Avg. Field Goal %", "Avg. 3 Point %", "Avg. 2 Point %"]]
+summary_per_chart.plot(kind="bar")
+plt.ylim(0, 0.75)
+plt.legend(loc="upper center", bbox_to_anchor=(0.5,1))
+plt.show()
